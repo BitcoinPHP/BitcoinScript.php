b4aeb96a5be9d451b4f42ec9acafed021b2d9e97
rgex <124898@supinfo.com>
2015-11-28 20:30:15 -0500

    wip

--- src/BitcoinPHP/BitcoinScript/ScriptBuilder.php
+++ src/BitcoinPHP/BitcoinScript/ScriptBuilder.php
@@ -60,43 +60,42 @@ class ScriptBuilder
             case 0:
                 return $this;
             case 1:
-                if(ord($data) === 0)
-                    array_push($this->script, array('opCode' => OpCodes::OP_FALSE));
-                else if(ord($data) === 1)
+                if(strlen($data) === 0)
+                    array_push($this->script, array('opCode' => OpCodes::OP_0));
+                else if(strlen($data) === 1)
                     array_push($this->script, array('opCode' => OpCodes::OP_1));
-                else if(ord($data) === 2)
+                else if(strlen($data) === 2)
                     array_push($this->script, array('opCode' => OpCodes::OP_2));
-                else if(ord($data) === 3)
+                else if(strlen($data) === 3)
                     array_push($this->script, array('opCode' => OpCodes::OP_3));
-                else if(ord($data) === 4)
+                else if(strlen($data) === 4)
                     array_push($this->script, array('opCode' => OpCodes::OP_4));
-                else if(ord($data) === 5)
+                else if(strlen($data) === 5)
                     array_push($this->script, array('opCode' => OpCodes::OP_5));
-                else if(ord($data) === 6)
+                else if(strlen($data) === 6)
                     array_push($this->script, array('opCode' => OpCodes::OP_6));
-                else if(ord($data) === 7)
+                else if(strlen($data) === 7)
                     array_push($this->script, array('opCode' => OpCodes::OP_7));
-                else if(ord($data) === 8)
+                else if(strlen($data) === 8)
                     array_push($this->script, array('opCode' => OpCodes::OP_8));
-                else if(ord($data) === 9)
+                else if(strlen($data) === 9)
                     array_push($this->script, array('opCode' => OpCodes::OP_9));
-                else if(ord($data) === 10)
+                else if(strlen($data) === 10)
                     array_push($this->script, array('opCode' => OpCodes::OP_10));
-                else if(ord($data) === 11)
+                else if(strlen($data) === 11)
                     array_push($this->script, array('opCode' => OpCodes::OP_11));
-                else if(ord($data) === 12)
+                else if(strlen($data) === 12)
                     array_push($this->script, array('opCode' => OpCodes::OP_12));
-                else if(ord($data) === 13)
+                else if(strlen($data) === 13)
                     array_push($this->script, array('opCode' => OpCodes::OP_13));
-                else if(ord($data) === 14)
+                else if(strlen($data) === 14)
                     array_push($this->script, array('opCode' => OpCodes::OP_14));
-                else if(ord($data) === 15)
+                else if(strlen($data) === 15)
                     array_push($this->script, array('opCode' => OpCodes::OP_15));
-                else if(ord($data) === 16)
+                else if(strlen($data) === 16)
                     array_push($this->script, array('opCode' => OpCodes::OP_16));
-                if(ord($data) < 17)
-                    return $this;
-                array_push($this->script, array('opCode' => OpCodes::OP_PUSHDATA1));
+                else
+                    array_push($this->script, array('opCode' => OpCodes::OP_PUSHDATA1));
             break;
 
             case 2:
@@ -108,7 +107,8 @@ class ScriptBuilder
             break;
         }
 
-        array_push($this->script, array('bin' => $dataLength));
+        if(strlen($data) > 17)
+            array_push($this->script, array('bin' => $dataLength));
         array_push($this->script, array('bin' => $data));
         return $this;
     }
